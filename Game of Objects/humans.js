0c9bf3355a57ec37a64456c85f148e2ada529743
NikolaSimunoski <47476124+NikolaSimunoski@users.noreply.github.com>
2019-03-01 17:04:43 +0100

    Update humans.js

--- Game of Objects/humans.js
+++ Game of Objects/humans.js
@@ -1,18 +1,11 @@
 function Knight(name) {
-    // energija na vitezot, koga ke dojde do 0 toj umira
     this.health = 1200;
-
-    // ova se vrednosti min/max za getRandom funkcijata
-    // za da ni generira random vrednsot na napadot
     this.minDamage = 50;
     this.maxDamage = 75;
-    
     this.name = name;
     this.type = "knight"
 }
 
-// Knight nasleduva od negoviot roditel Combatant
-// Nasleduvanjeto e prototipalno
 Knight.prototype = new Combatant();
 
 function Wizard(name) {

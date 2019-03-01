5caf7c3a742200515945ee15a601cf15f0ed26b2
NikolaSimunoski <47476124+NikolaSimunoski@users.noreply.github.com>
2019-03-01 17:04:11 +0100

    Update dragon.js

--- Game of Objects/dragon.js
+++ Game of Objects/dragon.js
@@ -1,13 +1,9 @@
 function Dragon(name) {
     this.name = name;
     this.health = 25000;
-
-    // dragonot pravi steta od min do max, na random
     this.minDamage = 500;
     this.maxDamage = 750;
-    
     this.type = "dragon";
 }
 
-// duri i dragonot nasleduva od combatant
 Dragon.prototype = new Combatant();
